051ab304a31d32d78711ffb78121616cd26dbfdd
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-18 15:44:45 +0200

    asking <> for inputs of percentage& C:/Users/moudi/AppData/Local/Programs/Python/Python313/python.exe c:/Users/moudi/Desktop/SEFactory/Projects/salary-calculator/salary.py

--- salary.py
+++ salary.py
@@ -1,4 +1,15 @@
 salary_amount=int(input("Salary amount :"))
 salary_month=input("The name of the month to store the salary :")
 salary_taxs=[]
+for tax in range (3):
+    if tax ==0:
+        savings = int(input("Savings percentage :"))
+        salary_taxs.append(savings)
+    elif tax ==1:
+        rent = int(input("Rent percentage :"))
+        salary_taxs.append(rent)
+
+    else :
+        electricty = int(input("Electricity percentage :"))
+        salary_taxs.append(electricty)
 
