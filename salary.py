5f706523af6777184f4388c219fea984d0ef860e
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-02-18 15:55:44 +0200

    calculating <> the amount of each tax

--- salary.py
+++ salary.py
@@ -12,4 +12,13 @@ for tax in range (3):
     else :
         electricty = int(input("Electricity percentage :"))
         salary_taxs.append(electricty)
+sum = 0 
+for i in range (len(salary_taxs)):
+
+    if i == 0 :
+        print (f"the amount for the month {salary_month} is {int(salary_amount*salary_taxs[i]/100)} for the savings ")
+    elif i ==1:
+        print (f"the amount for the month {salary_month} is {int(salary_amount*salary_taxs[i]/100)} for the rent ")
+    else :
+        print(f"the amount for the month {salary_month} is {int(salary_amount*salary_taxs[i]/100)} for the electricty ")
 
